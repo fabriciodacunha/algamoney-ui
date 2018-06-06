813403fc7c4f8b789175afebe13f08c7483952f1
Fabrício da Cunha Oliveira <fabricio.cunha@mpro.mp.br>
2018-06-06 16:56:24 -0400

    Inserindo --locale pt-BR 2

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,5 +1,5 @@
 export const environment = {
   production: true,
-  // apiUrl: 'https://fabriciomoney-api.herokuapp.com'
-  apiUrl: 'http://localhost:8080'
+  apiUrl: 'https://fabriciomoney-api.herokuapp.com'
+  // apiUrl: 'http://localhost:8080'
 };
